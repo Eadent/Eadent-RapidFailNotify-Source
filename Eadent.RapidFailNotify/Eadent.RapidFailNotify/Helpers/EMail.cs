cd8a54cbec6801179661fbed809c0e978dc5f070
Éamonn Duffy <Eamonn@Duffy.global>
2021-04-27 22:09:36 +0100

    feat: First Iteration Of An IIS Rapid Fail Notify Application.
    https://www.Eadent.com/

---
+++ Eadent.RapidFailNotify/Eadent.RapidFailNotify/Helpers/EMail.cs
@@ -0,0 +1,39 @@
+﻿using Eadent.RapidFailNotify.DataTransferObjects;
+using MailKit.Net.Smtp;
+using MimeKit;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Eadent.RapidFailNotify.Helpers
+{
+    public static class EMail
+    {
+        public static void Send(string toEMailName, string toEMailAddress, string subject, string htmlBody)
+        {
+            string eMailSettingsFilePath = "App_Data/Confidential/E-Mail.settings.json";  // TODO: Consider obtaining from Configuration.
+
+            string eMailSettingsString = File.ReadAllText(eMailSettingsFilePath);
+
+            EMailSettingsDto eMailSettings = JsonConvert.DeserializeObject<EMailSettingsDto>(eMailSettingsString);
+
+            var mailMessage = new MimeMessage();
+
+            mailMessage.From.Add(new MailboxAddress(eMailSettings.FromEMailName, eMailSettings.FromEMailAddress));
+            mailMessage.To.Add(new MailboxAddress(toEMailName, toEMailAddress));
+            mailMessage.Subject = subject;
+            mailMessage.Body = new TextPart("html")
+            {
+                Text = htmlBody
+            };
+
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect(eMailSettings.HostName, eMailSettings.HostPort, true);
+                smtpClient.Authenticate(eMailSettings.SenderEMailAddress, eMailSettings.SenderPassword);
+                smtpClient.Send(mailMessage);
+                smtpClient.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ Eadent.RapidFailNotify/Eadent.RapidFailNotify/Program.cs
@@ -0,0 +1,58 @@
+﻿using Eadent.RapidFailNotify.Helpers;
+using System;
+
+namespace Eadent.RapidFailNotify
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int exitCode = 0;
+
+            string applicationNamePool = null;
+            string eMailName = null;
+            string eMailAddress = null;
+
+            if (args.Length < 6)
+            {
+                exitCode = -1;
+            }
+            else
+            {
+                if (string.Equals(args[0], "-ApplicationPoolName", StringComparison.OrdinalIgnoreCase))
+                {
+                    applicationNamePool = args[1];
+                }
+
+                if (string.Equals(args[2], "-EMailName", StringComparison.OrdinalIgnoreCase))
+                {
+                    eMailName = args[3];
+                }
+
+                if (string.Equals(args[4], "-EMailAddress", StringComparison.OrdinalIgnoreCase))
+                {
+                    eMailAddress = args[5];
+                }
+
+                if ((applicationNamePool == null) || (eMailName == null) || (eMailAddress == null))
+                {
+                    exitCode = -2;
+                }
+                else
+                {
+                    DateTime utcNow = DateTime.UtcNow;
+
+                    string htmlBody = $"The IIS Rapid Fail Protection has been invoked.<br><br>" +
+                        $"Application Pool Name: <strong>{applicationNamePool}</strong><br><br>" +
+                        $"Machine Name: <strong>{Environment.MachineName}</strong><br><br>" +
+                        $"Date & Time (UTC): <strong>{utcNow:dddd, d-MMM-yyyy h:mm:ss tt}</strong><br><br>" +
+                        $"Date & Time (Local): <strong>{utcNow.ToLocalTime():dddd, d-MMM-yyyy h:mm:ss tt}</strong>";
+
+                    EMail.Send(eMailName, eMailAddress, $"IIS Rapid Fail Protection: {applicationNamePool}", htmlBody);
+                }
+            }
+
+            return exitCode;
+        }
+    }
+}
